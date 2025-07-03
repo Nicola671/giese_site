15139b92464eed3ce80cecffb10782cb44301523
nicoals <nicolasmedinae06@gmail.com>
2025-07-03 15:34:37 -0300

    Agrego dominio de Render a ALLOWED_HOSTS

--- giese_site/settings.py
+++ giese_site/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-reemplaza-esto-por-una-clave-segura'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['giese-site.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
